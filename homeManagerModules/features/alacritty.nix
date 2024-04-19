b047ed3c88f3ed69e4f3f27cbe941537810d733e
Alexandra Østermark <alex.cramt@gmail.com>
2024-04-19 23:45:53 +0200

    zellij working

--- homeManagerModules/features/alacritty.nix
+++ homeManagerModules/features/alacritty.nix
@@ -1,5 +1,11 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.alacritty = {
     enable = true;
+    settings = {
+      shell = {
+        program = "${pkgs.zsh}/bin/zsh";
+        args = [ "-l" "-c" "${pkgs.zellij}/bin/zellij attach --index 0 || ${pkgs.zellij}/bin/zellij" ];
+      };
+    };
   };
 }
