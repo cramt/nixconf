af816137abcd4e9fa3dfe9820b0e2319119a3390
Alexandra Østermark <alex.cramt@gmail.com>
2024-04-21 16:57:08 +0200

    alacritty fixed

--- homeManagerModules/features/alacritty.nix
+++ homeManagerModules/features/alacritty.nix
@@ -6,6 +6,20 @@
         program = "${pkgs.zsh}/bin/zsh";
         args = [ "-l" "-c" "${pkgs.zellij}/bin/zellij attach --index 0 || ${pkgs.zellij}/bin/zellij" ];
       };
+      font = {
+        size = 8.5;
+      };
+      colors = {
+        draw_bold_text_with_bright_colors = true;
+      };
+      mouse = {
+        hide_when_typing = true;
+      };
+      window = {
+        opacity = 0.6;
+        decorations_theme_variant = "Dark";
+        dynamic_padding = true;
+      };
     };
   };
 }
