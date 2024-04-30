3d8d716aa8b611a4ac06ab99995e369d96d186ff
Alexandra Østermark <alex.cramt@gmail.com>
2024-04-30 17:43:43 +0200

    aa

--- nixosModules/features/nvidia.nix
+++ nixosModules/features/nvidia.nix
@@ -30,6 +30,7 @@ in
       __GLX_VENDOR_LIBRARY_NAME = "nvidia";
       LIBVA_DRIVER_NAME = "nvidia";
       __NV_PRIME_RENDER_OFFLOAD = "1";
+      __NV_PRIME_RENDER_OFFLOAD_PROVIDER = "NVIDIA-G0";
       __VK_LAYER_NV_optimus = "NVIDIA_only";
     };
     hardware.opengl = {
