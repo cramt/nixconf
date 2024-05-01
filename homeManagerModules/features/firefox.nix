5a8a827ac9626d02440dfe3afc8f5c9ed23e1e7a
Alexandra Østermark <alex.cramt@gmail.com>
2024-05-01 13:06:32 +0200

    add mars

--- homeManagerModules/features/firefox.nix
+++ homeManagerModules/features/firefox.nix
@@ -1,34 +1,52 @@
 { inputs
 , pkgs
+, config
+, lib
 , ...
 }:
+let
+  cfg = config.myHomeManager.firefox;
+in
 {
-  programs.firefox = {
-    enable = true;
-    profiles.cramt = {
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-        ublock-origin
-        sponsorblock
-        vimium
-        dashlane
-        dracula-dark-colorscheme
-        widegithub
-      ];
+  options.myHomeManager.firefox = {
+    profiles = lib.mkOption {
+      default = {
+        cramt = {
+          extensions = with pkgs.nur.repos.rycee.firefox-addons;
+            [
+              dashlane
+              ublock-origin
+              sponsorblock
+              vimium
+              dracula-dark-colorscheme
+              widegithub
+            ];
 
-      search.force = true;
+          search.force = true;
 
-      settings = {
-        "browser.disableResetPrompt" = true;
-        "browser.download.panel.shown" = true;
-        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-        "browser.shell.checkDefaultBrowser" = false;
-        "browser.shell.defaultBrowserCheckCount" = 1;
-        "browser.uiCustomization.state" = ''{"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","library-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"],"currentVersion":18,"newElementCount":4}'';
-        "dom.security.https_only_mode" = true;
-        "identity.fxaccounts.enabled" = false;
-        "privacy.trackingprotection.enabled" = true;
-        "signon.rememberSignons" = false;
+          settings = {
+            "browser.disableResetPrompt" = true;
+            "browser.download.panel.shown" = true;
+            "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+            "browser.shell.checkDefaultBrowser" = false;
+            "browser.shell.defaultBrowserCheckCount" = 1;
+            "browser.uiCustomization.state" = ''{"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","library-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"],"currentVersion":18,"newElementCount":4}'';
+            "dom.security.https_only_mode" = true;
+            "identity.fxaccounts.enabled" = false;
+            "privacy.trackingprotection.enabled" = true;
+            "signon.rememberSignons" = false;
+          };
+        };
       };
+      description = ''
+        additional profiles
+      '';
+    };
+  };
+  config = {
+    programs.firefox = {
+      enable = true;
+      profiles = cfg.profiles;
     };
   };
 }
--- hosts/mars/home.nix
+++ hosts/mars/home.nix
@@ -12,7 +12,69 @@
   myHomeManager = {
     bundles.general.enable = true;
     bundles.graphical.enable = true;
+    bundles.work.enable = true;
     git.signingKey = "5A2AFD974351E6CA";
+    firefox.profiles = {
+      cramt = {
+        extensions = with pkgs.nur.repos.rycee.firefox-addons;
+          [
+            dashlane
+            ublock-origin
+            sponsorblock
+            vimium
+            dracula-dark-colorscheme
+            widegithub
+          ];
+
+        search.force = true;
+
+        isDefault = false;
+
+        id = 0;
+
+        settings = {
+          "browser.disableResetPrompt" = true;
+          "browser.download.panel.shown" = true;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+          "browser.shell.checkDefaultBrowser" = false;
+          "browser.shell.defaultBrowserCheckCount" = 1;
+          "browser.uiCustomization.state" = ''{"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","library-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"],"currentVersion":18,"newElementCount":4}'';
+          "dom.security.https_only_mode" = true;
+          "identity.fxaccounts.enabled" = false;
+          "privacy.trackingprotection.enabled" = true;
+          "signon.rememberSignons" = false;
+        };
+      };
+      work = {
+        extensions = with pkgs.nur.repos.rycee.firefox-addons;
+          [
+            ublock-origin
+            sponsorblock
+            vimium
+            dracula-dark-colorscheme
+            widegithub
+          ];
+
+        search.force = true;
+
+        isDefault = true;
+
+        id = 1;
+
+        settings = {
+          "browser.disableResetPrompt" = true;
+          "browser.download.panel.shown" = true;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+          "browser.shell.checkDefaultBrowser" = false;
+          "browser.shell.defaultBrowserCheckCount" = 1;
+          "browser.uiCustomization.state" = ''{"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","home-button","urlbar-container","downloads-button","library-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button","ublock0_raymondhill_net-browser-action","_testpilot-containers-browser-action"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar","widget-overflow-fixed-list"],"currentVersion":18,"newElementCount":4}'';
+          "dom.security.https_only_mode" = true;
+          "identity.fxaccounts.enabled" = false;
+          "privacy.trackingprotection.enabled" = true;
+          "signon.rememberSignons" = false;
+        };
+      };
+    };
     sway.monitors = {
       eDP-1 = {
         pos = "0 0";
