eb694f432c10c41e8f1582cd1987e2d517f368e9
Alexandra Østermark <alex.cramt@gmail.com>
2024-06-21 23:23:09 +0200

    start background steam

--- nixosModules/features/steam.nix
+++ nixosModules/features/steam.nix
@@ -13,4 +13,14 @@
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
   };
   programs.gamemode.enable = true;
+  systemd.user.services.steam_background = {
+    enable = true;
+    description = "Open Steam in the background at boot";
+    wantedBy = [ "graphical-session.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.steam}/bin/steam -nochatui -nofriendsui -silent %U";
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+  };
 }
