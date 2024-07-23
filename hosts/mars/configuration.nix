ec2568f5c97215c14d340e3424b674786be46234
Alexandra Østermark <alex.cramt@gmail.com>
2024-07-23 16:50:03 +0200

    add other ips

--- hosts/mars/configuration.nix
+++ hosts/mars/configuration.nix
@@ -60,7 +60,7 @@
 
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.settings.trusted-substituters = [ "192.168.0.103:5000" "https://cache.nixos.org/" ];
+  nix.settings.trusted-substituters = [ "192.168.0.103:5000" "192.168.0.107:5000" "https://cache.nixos.org/" ];
   environment.systemPackages = [
   ];
 
