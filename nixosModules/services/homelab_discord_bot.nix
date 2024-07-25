12aa37d5562ec5e2dd57656c358f2ef42213d154
Alexandra Østermark <alex.cramt@gmail.com>
2024-07-25 12:19:01 +0200

    stuff

--- nixosModules/services/homelab_discord_bot.nix
+++ nixosModules/services/homelab_discord_bot.nix
@@ -14,9 +14,18 @@ in
   config = {
     systemd.services.homelab_discord_bot = {
       enable = true;
-      script = ''
-        DATABASE_URL=${cfg.databaseUrl} DISCORD_TOKEN=$(cat ${config.sops.secrets."homelab_discord_bot/discord_token".path}) ${inputs.homelab_discord_bot.packages.${pkgs.system}.homelab_discord_bot}/bin/homelab_discord_bot
-      '';
+      script =
+        let
+          envs = {
+            DATABASE_URL = cfg.databaseUrl;
+            DISCORD_TOKEN = "$(cat ${config.sops.secrets."homelab_discord_bot/discord_token".path})";
+            ALLOWED_GUILD = "$(cat ${config.sops.secrets."homelab_discord_bot/allowed_guild".path})";
+            SYSTEMCTL_PATH = "${pkgs.systemd}/bin/systemctl";
+          };
+          envCommand = lib.strings.concatStringsSep " " (lib.attrsets.mapAttrsToList (name: value: "${name}=${value}") envs);
+          binary = "${inputs.homelab_discord_bot.packages.${pkgs.system}.homelab_discord_bot}/bin/homelab_discord_bot";
+        in
+        "${envCommand} ${binary}";
       description = "Runs homelab discord bot";
       wantedBy = [ "network-online.target" ];
       serviceConfig = {
