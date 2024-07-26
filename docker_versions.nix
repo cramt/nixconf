1443e57324ad2d94220e2cfed98cd99023b49bf8
Alexandra Østermark <alex.cramt@gmail.com>
2024-07-26 14:16:26 +0200

    a

--- docker_versions.nix
+++ docker_versions.nix
@@ -1,10 +1,10 @@
 {
   jellyfin = "10.9.8";
-  prowlarr = "1.21.2";
+  prowlarr = "1.21.1";
   sonarr = "4.0.8";
   radarr = "5.9.0";
   caddy = "2.8.4";
-  bazarr = "1.4.4";
+  bazarr = "1.4.3";
   qbittorrent = "4.6.5";
   adguard = "v0.107.52";
   tor-privoxy = "latest";
