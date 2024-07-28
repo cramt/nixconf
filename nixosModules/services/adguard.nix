8b098ca5c06e26db40047ffdea7417f3bd2379da
Alexandra Østermark <alex.cramt@gmail.com>
2024-07-28 12:16:05 +0200

    try this

--- nixosModules/services/adguard.nix
+++ nixosModules/services/adguard.nix
@@ -20,7 +20,10 @@ in
       };
     };
   config = {
-    networking.firewall.allowedUDPPorts = [ 53 ];
+    networking.firewall = {
+      allowedUDPPorts = [ 53 ];
+      allowedTCPPorts = [ 53 ];
+    };
     virtualisation.oci-containers.containers.adguard = {
       hostname = "adguard";
       image = "adguard/adguardhome:${docker_versions.adguard}";
