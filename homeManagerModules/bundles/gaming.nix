3dbbd3282f90a00df34b003894d2c3c5cda0a255
Alexandra Østermark <alex.cramt@gmail.com>
2024-08-27 19:24:11 +0200

    stuff

--- homeManagerModules/bundles/gaming.nix
+++ homeManagerModules/bundles/gaming.nix
@@ -21,4 +21,8 @@
     wowup.enable = true;
     cockatrice.enable = true;
   };
+
+  # this is to make "wine system tray" not show up as a seperate stupid window
+  # delete when can cause it pulls in a shitton of kde stuff
+  services.xembed-sni-proxy.enable = true;
 }
