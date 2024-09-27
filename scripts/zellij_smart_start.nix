215c8dbf273abe6f7960ac7fa32301c64c17a74d
Alexandra Østermark <alex.cramt@gmail.com>
2024-09-27 10:44:53 +0200

    whoopsie

--- scripts/zellij_smart_start.nix
+++ scripts/zellij_smart_start.nix
@@ -2,7 +2,7 @@
   let attched_sessions = ps --long | where command =~ "zellij --server" | each { 
     let value = $in.command | split row "/" | last | str replace "main" ""
     try { $value | into int }
-  } | sort | enumerate | where $in.index == $in.item | get item
+  } | sort | enumerate | where { $in.index == $in.item } | get item
   let new_index = try { ($attched_sessions | last) + 1 } catch { 0 }
   ${pkgs.zellij}/bin/zellij attach $"main($new_index)" --create
 ''
