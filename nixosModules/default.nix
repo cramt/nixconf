a31e585fcaa3c5ecebfaa80abd96a79a28219146
Alexandra Østermark <alex.cramt@gmail.com>
2025-01-07 11:39:39 +0100

    cosmic????

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -78,10 +78,12 @@ in {
         "https://walker.cachix.org"
         "https://walker-git.cachix.org"
         "https://cosmic.cachix.org"
+        "https://cosmic.cachix.org"
       ];
       trusted-public-keys = [
         "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
         "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
+        "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
         "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
       ];
     };
