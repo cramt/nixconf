966d7bc9a823a1130c456f532dd5b84f9b7b7f3d
Alexandra Østermark <alex.cramt@gmail.com>
2025-01-07 15:52:41 +0100

    yooo cosmic

--- hosts/saturn/configuration.nix
+++ hosts/saturn/configuration.nix
@@ -119,17 +119,9 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
-  services.displayManager.ly = {
-    enable = false;
-    #settings = {
-    #animation = "matrix";
-    #bigclock = "fa";
-    #hide_borders = true;
-    #};
-  };
+  #services.xserver.displayManager.gdm.enable = true;
+  services.desktopManager.cosmic.enable = true;
+  services.displayManager.cosmic-greeter.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
