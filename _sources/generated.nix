d1ea7d1a6b18361af66349b094fd1982fb984c3a
Alexandra Østermark <alex.cramt@gmail.com>
2025-03-10 08:58:11 +0100

    updaty

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -16,8 +16,8 @@
     version = "1.5.1";
     src = dockerTools.pullImage {
       imageName = "linuxserver/bazarr";
-      imageDigest = "sha256:ac9fe56bee9133bcb9e27fe48faaf83c57b83d75bacc277d9b2619136632b1fe";
-      sha256 = "sha256-xSr7YKWSHB1Ma7o1eEInGF4yD8g097kqRUUCNVxVCvc=";
+      imageDigest = "sha256:8415caab20c3642adb281ea066e82a58b8011b6e12bec5339db66599b700f805";
+      sha256 = "sha256-7mABS8PqgYktqyhr4I2gnLqF0wMwm/WLYH8MQf3UR4Q=";
       finalImageTag = "1.5.1";
     };
   };
@@ -26,19 +26,19 @@
     version = "2.9.1";
     src = dockerTools.pullImage {
       imageName = "library/caddy";
-      imageDigest = "sha256:2c136eb7e4daa97deed8738aab21994ea88dc0ced06aa44e30a949ba3d60e213";
+      imageDigest = "sha256:a863d46cf06a9084f36cbffbe9f4ad046971dca32f79c68129aaf15ad356d6ce";
       sha256 = "sha256-BjLwNrXPZFwZs7Fau/pxhUPSImBT8ms+nUVBs1PO4H8=";
       finalImageTag = "2.9.1";
     };
   };
   jellyfin = {
     pname = "jellyfin";
-    version = "2025030305";
+    version = "2025031005";
     src = dockerTools.pullImage {
       imageName = "jellyfin/jellyfin";
-      imageDigest = "sha256:d9c8cc2c10329278e86272a1184d393cc5fcdd62f773bd0fd5575fb7fcd7b966";
-      sha256 = "sha256-Z2W1T4N6oCLPpzsHBm+trElMEN3WWikkVdH1khFdnuU=";
-      finalImageTag = "2025030305";
+      imageDigest = "sha256:7204129fa86760cf165fe6e8aac11e82ecc11f308f539ec6ff7bf2071dfbfc2f";
+      sha256 = "sha256-wRaK9BD0+oUXi/KlUlCUXHt6/dfhwbCIDzqHiJYf5bE=";
+      finalImageTag = "2025031005";
     };
   };
   minecraft-server = {
@@ -46,8 +46,8 @@
     version = "java8";
     src = dockerTools.pullImage {
       imageName = "itzg/minecraft-server";
-      imageDigest = "sha256:40083af30bd9d75358ba14a825f3c1d3620875be0c055b212c9aed76420a9d54";
-      sha256 = "sha256-ve8kNL+OKdwuONuN+SWRha6T4G5g5amjg+idFi4+2Wo=";
+      imageDigest = "sha256:b70787a42a14867669d66c900b5c42bbaf9d2a21c7e0edd29b9c0e050c95b6ea";
+      sha256 = "sha256-V1M5cd2IoxZcm+xOle+CdCCFN0E+uujRMbTbT5fIqWA=";
       finalImageTag = "java8";
     };
   };
