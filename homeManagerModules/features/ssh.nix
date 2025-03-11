a93cf83110d9b63a5d6e41442ef50d0d7a3321f7
Alexandra Østermark <alex.cramt@gmail.com>
2025-03-11 15:21:13 +0100

    a

--- homeManagerModules/features/ssh.nix
+++ homeManagerModules/features/ssh.nix
@@ -1,20 +1,20 @@
-{ pkgs, ... }:
-let
+{pkgs, ...}: let
   sshTargets = {
-    luna = "-t cramt@192.168.0.103 zellij_smart_start";
-    remote_luna = "-t cramt@84.238.30.251 -p 2269 zellij_smart_start";
+    luna = "-t cramt@192.168.0.103 -A zellij_smart_start";
+    remote_luna = "-t cramt@84.238.30.251 -p 2269 -A zellij_smart_start";
     jump = "ao@161.35.219.109 -A";
   };
   sshTargetPackages = builtins.mapAttrs (name: value: pkgs.writeScriptBin "ssh_${name}" "ssh ${value}") sshTargets;
-  sshTargetDesktops = builtins.mapAttrs
-    (name: value: pkgs.makeDesktopItem {
-      name = "ssh-${name}";
-      desktopName = "ssh ${name}";
-      exec = "${pkgs.alacritty}/bin/alacritty -e ${pkgs.zsh}/bin/zsh -c ${value}/bin/ssh_${name}";
-    })
+  sshTargetDesktops =
+    builtins.mapAttrs
+    (name: value:
+      pkgs.makeDesktopItem {
+        name = "ssh-${name}";
+        desktopName = "ssh ${name}";
+        exec = "${pkgs.alacritty}/bin/alacritty -e ${pkgs.zsh}/bin/zsh -c ${value}/bin/ssh_${name}";
+      })
     sshTargetPackages;
-in
-{
+in {
   programs.ssh = {
     enable = true;
     addKeysToAgent = "yes";
