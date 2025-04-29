d8c7fb01945f84aef4f814c6f8fcdbabbe1b1557
Alexandra Østermark <alex.cramt@gmail.com>
2025-04-29 22:18:54 +0200

    stuff

--- hosts/saturn/configuration.nix
+++ hosts/saturn/configuration.nix
@@ -118,7 +118,7 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  services.xserver.displayManager.gdm.enable = true;
+  services.displayManager.cosmic-greeter.enable = true;
   services.desktopManager.cosmic.enable = true;
 
   # Configure keymap in X11
---
+++ nixosModules/services/conduit.nix
@@ -0,0 +1,64 @@
+{
+  pkgs,
+  config,
+  ...
+}: let
+  secrets = import ../../secrets.nix;
+  shared_secret = secrets.shared_matrix_secret;
+  turnDomain = "turn.${secrets.domain}";
+  turnMin = 49000;
+  turnMax = 50000;
+  cloudflareCredentials = pkgs.writeText ''cloudflare_creds.env'' ''
+    CLOUDFLARE_EMAIL=${secrets.email}
+    CLOUDFLARE_API_KEY=${secrets.cloudflare_api_key}
+  '';
+in {
+  config = {
+    # tls
+    security.acme.acceptTerms = true;
+    security.acme.certs.${turnDomain} = {
+      reloadServices = ["coturn"];
+      group = "turnserver";
+      email = secrets.email;
+      dnsProvider = "cloudflare";
+      environmentFile = cloudflareCredentials;
+    };
+    # turn server notwork config
+    networking.firewall = {
+      allowedUDPPortRanges = [
+        {
+          from = turnMin;
+          to = turnMax;
+        }
+      ];
+      allowedUDPPorts = [3478 5349];
+      allowedTCPPortRanges = [];
+      allowedTCPPorts = [3478 5349];
+    };
+    services = {
+      matrix-conduit = {
+        enable = true;
+        settings.global = {
+          server_name = "matrix.${secrets.domain}";
+          turn_uris = ["turn:${turnDomain}:3478?transport=udp" "turn:${turnDomain}:3478?transport=tcp"];
+          turn_secret = shared_secret;
+          allow_check_for_updates = false;
+          database_backend = "rocksdb";
+        };
+      };
+
+      coturn = {
+        enable = true;
+        no-cli = true;
+        no-tcp-relay = true;
+        min-port = turnMin;
+        max-port = turnMax;
+        use-auth-secret = true;
+        static-auth-secret = shared_secret;
+        realm = turnDomain;
+        cert = "${config.security.acme.certs.${turnDomain}.directory}/full.pem";
+        pkey = "${config.security.acme.certs.${turnDomain}.directory}/key.pem";
+      };
+    };
+  };
+}
