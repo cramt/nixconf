0391982ee0d2283afe3236365a67565c92344be5
Alexandra Østermark <alex.cramt@gmail.com>
2025-06-19 20:17:17 +0200

    a

--- gems/gemset.nix
+++ gems/gemset.nix
@@ -149,10 +149,10 @@
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "1cxyg49nhy137hfjprqwb4h29gmpnwb8yif6qza6p6dgavm98p77";
+      sha256 = "1pn8w91d6iwj3pf0pfll1j08pir4nbw8c0b17srj6yk2r467xmbq";
       type = "gem";
     };
-    version = "0.24.1";
+    version = "0.24.2";
   };
   ruby-progressbar = {
     groups = ["default"];
@@ -169,10 +169,10 @@
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "1jzi9qqxa1dx4c141ckmc3h3qf016x23y7zg1sknksbaaclxzbif";
+      sha256 = "0hfxshkcmqarj60yyyfkx74pd75bac659hdzpdlhxrqd0gccn66d";
       type = "gem";
     };
-    version = "0.5.12189";
+    version = "0.5.12194";
   };
   unicode-display_width = {
     dependencies = ["unicode-emoji"];
