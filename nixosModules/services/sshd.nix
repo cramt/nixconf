f328f30e76b5f00d3ed5dfe012224d183b74904a
Alexandra Østermark <alex.cramt@gmail.com>
2025-08-12 20:59:06 +0200

    a

--- nixosModules/services/sshd.nix
+++ nixosModules/services/sshd.nix
@@ -1,4 +1,14 @@
-{ ... }: {
+{
+  config,
+  lib,
+  ...
+}: {
+  nix.sshServe = {
+    enable = true;
+    write = true;
+    trusted = true;
+    keys = lib.lists.flatten (lib.attrsets.mapAttrsToList (name: user: user.authorizedKeys) config.myNixOS.home-users);
+  };
   services.openssh = {
     enable = true;
     settings = {
