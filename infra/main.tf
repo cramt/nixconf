be31c1dcc860243a8cac4f45c29ac50307f473f5
Alexandra Østermark <alex.cramt@gmail.com>
2025-09-12 19:17:35 +0200

    a

--- infra/main.tf
+++ infra/main.tf
@@ -36,3 +36,32 @@ resource "cloudflare_dns_record" "atproto_hannah" {
   ttl     = 1
   type    = "TXT"
 }
+
+resource "cloudflare_dns_record" "github_hannah" {
+  zone_id = local.zone_id
+  content = "6eebf91a394916de349e9b7bb71a54"
+  name    = "_github-pages-challenge-HannahField.hannah.${local.secrets.domain}"
+  proxied = false
+  ttl     = 1
+  type    = "TXT"
+}
+
+resource "cloudflare_dns_record" "github_pages_hannah" {
+  for_each = toset(["185.199.108.153", "185.199.109.153", "185.199.110.153", "185.199.111.153"])
+  zone_id  = local.zone_id
+  content  = each.value
+  name     = "hannah.${local.secrets.domain}"
+  proxied  = true
+  ttl      = 1
+  type     = "A"
+}
+
+resource "cloudflare_dns_record" "github_pages_hannah_ipv6" {
+  for_each = toset(["2606:50c0:8000::153", "2606:50c0:8001::153", "2606:50c0:8002::153", "2606:50c0:8003::153"])
+  zone_id  = local.zone_id
+  content  = each.value
+  name     = "hannah.${local.secrets.domain}"
+  proxied  = true
+  ttl      = 1
+  type     = "AAAA"
+}
