062bea451b0a1dd18167fcf6a1d220b6cd0689bf
Alexandra Østermark <alex.cramt@gmail.com>
2026-02-26 15:28:52 +0100

    a

--- nixosModules/services/sunshine.nix
+++ nixosModules/services/sunshine.nix
@@ -1,8 +1,16 @@
-{...}: {
+{
+  pkgs,
+  inputs,
+  ...
+}: let
+  # TODO: remove once sunshine builds on unstable
+  pkgs-stable = inputs.nixpkgs-stable.legacyPackages.${pkgs.system};
+in {
   services.sunshine = {
     enable = true;
     autoStart = true;
     capSysAdmin = true;
     openFirewall = true;
+    package = pkgs-stable.sunshine;
   };
 }
